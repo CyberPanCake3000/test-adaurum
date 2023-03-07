b2e937cee60db5df3a3962488a4410de800f7212
egatapova <gatapova.workspace21@gmail.com>
2023-03-08 01:09:08 +0300

    main project models

---
+++ database/migrations/2023_03_07_212538_create_companies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('inn');
+            $table->tinyText('info');
+            $table->string('ceo');
+            $table->string('address');
+            $table->string('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
